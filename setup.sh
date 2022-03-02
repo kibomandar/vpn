b73f29ccc2f07dfd10450d123428ea04f51b5453
kibomandar <97572235+kibomandar@users.noreply.github.com>
2022-03-03 06:33:46 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -27,19 +27,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
-clear
 fi
-echo '============================================='
-echo '          Sila Tunggu sebentar '
-echo 'Process Update & Upgrade Sedang Dijalankan '
-echo '============================================='
-sleep 2
-apt update && apt upgrade -y
-clear
-echo '============================================='
-echo '      Process Update&Upgrade Selesai '
-echo '============================================='
-sleep 2
 clear
 echo '============================================='
 echo '        Process Seterusnya Sila Tunggu '
