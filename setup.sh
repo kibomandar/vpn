7aaebae33380cc78ee5e8b52d060318f96e0aba3
kibomandar <97572235+kibomandar@users.noreply.github.com>
2022-03-03 06:48:53 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,12 +22,12 @@ fi
 if [ "$(systemd-detect-virt)" == "openvz" ]; then
 		echo "OpenVZ is not supported"
 		exit 1
-fi
+
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
-fi
+
 clear
 echo '============================================='
 echo '        Process Seterusnya Sila Tunggu '
