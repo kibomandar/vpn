8ed66454b6c6941cb511029b08a4164c8f8ae1b6
kibomandar <97572235+kibomandar@users.noreply.github.com>
2022-03-03 02:37:56 +0800

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -16,66 +16,27 @@ if [ -f "/etc/v2ray/domain" ]; then
 echo "Script Already Installed"
 exit 0
 fi
-echo '============================================='
-echo '          Sila Tunggu sebentar '
-echo 'Process Update & Upgrade Sedang Dijalankan '
-echo '============================================='
-sleep 2
-apt update && apt upgrade -y
-clear
-echo '============================================='
-echo '      Process Update&Upgrade Selesai '
-echo '============================================='
-sleep 2
-clear
-echo '============================================='
-echo '        Process Seterusnya Sila Tunggu '
-echo '============================================='
-sleep 2
-echo '============================================='
-echo "Sila Masukkan DOMAIN, Jika TIADA KLIK Enter"
-echo '============================================='
-echo '============================================='
-echo '        Installing SSH & OPENVPN '
-echo '============================================='
+mkdir /var/lib/premium-script;
+echo "IP=" >> /var/lib/premium-script/ipvps.conf
+wget https://raw.githubusercontent.com/kibomandar/vpn/main/cf.sh && chmod +x cf.sh && ./cf.sh
+#install ssh ovpn
 wget https://raw.githubusercontent.com/kibomandar/vpn/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
-echo '============================================='
-echo '        Installing SSTP '
-echo '============================================='
 wget https://raw.githubusercontent.com/kibomandar/vpn/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
-echo '============================================='
-echo '        Installing ssr '
-echo '============================================='
+#install ssr
 wget https://raw.githubusercontent.com/kibomandar/vpn/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
-echo '============================================='
-echo '        Installing shadowsocksobfs '
-echo '============================================='
 wget https://raw.githubusercontent.com/kibomandar/vpn/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
-echo '============================================='
-echo '        Installing WIREGUARD '
-echo '============================================='
+#installwg
 wget https://raw.githubusercontent.com/kibomandar/vpn/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
-echo '============================================='
-echo '        Installing V2RAY '
-echo '============================================='
+#install v2ray
 wget https://raw.githubusercontent.com/kibomandar/vpn/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
-echo '============================================='
-echo '        Installing ipsec '
-echo '============================================='
+#install L2TP
 wget https://raw.githubusercontent.com/kibomandar/vpn/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
-echo '        Installing XRAY '
-echo '============================================='
-wget https://raw.githubusercontent.com/kibomandar/vpn/main/install-xray.sh && chmod +x install-xray.sh && screen -S xray ./install-xray.sh
-echo '============================================='
-echo '        Installing Websocket '
-echo '============================================='
-wget https://raw.githubusercontent.com/kibomandar/vpn/main/websocket/websocket.sh && chmod +x websocket.sh && ./websocket.sh
-echo '        Installing OHP '
-echo '============================================='
-wget https://raw.githubusercontent.com/kibomandar/vpn/main/ohp/ohp.sh && chmod +x ohp.sh && ./ohp.sh
 wget https://raw.githubusercontent.com/kibomandar/vpn/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
-wget ⁸6https://raw.githubusercontent.com/kibomandar/vpn/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
-echo '============================================='
+# Install Neofetch 
+wget https://raw.githubusercontent.com/kibomandar/vpn/main/geo.sh && chmod +x geo.sh && geo.sh && dos2unix /usr/bin/geo.sh
+#install edu
+wget https://raw.githubusercontent.com/kibomandar/vpn/main/websocket.sh && chmod +x websocket.sh && ./websocket.sh
+wget https://raw.githubusercontent.com/kibomandar/vpn/main/edu.sh && chmod +x edu.sh && screen -S edu ./edu.sh
 
 rm -f /root/ssh-vpn.sh
 rm -f /root/sstp.sh
@@ -83,44 +44,47 @@ rm -f /root/wg.sh
 rm -f /root/ss.sh
 rm -f /root/ssr.sh
 rm -f /root/ins-vt.sh
-rm -f /root/install-xray.sh
 rm -f /root/ipsec.sh
-rm -f /root/websocket.sh
-rm -f /root/ohp.sh
 rm -f /root/set-br.sh
-rm -f /root/nf
-echo "1.1" > /home/ver
+rm -f /root/geo.sh
+rm -f /root/websocket.sh
+rm -f /root/edu.sh
+cat <<EOF> /etc/systemd/system/autosett.service
+[Unit]
+Description=autosetting
+Documentation=https://joysmark.me
 
-echo "echo '      ꧁DEAR MY LOVE ,Hanya kerna sekali dikhianati꧂             '" >> .profile
-echo "echo '          ꧁Aku Menjadi Tak Kenal Apa itu Cinta꧂                 '" >> .profile
+[Service]
+Type=oneshot
+ExecStart=/bin/bash /etc/set.sh
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl enable autosett
+wget -O /etc/set.sh "https://raw.githubusercontent.com/kibomandar/vpn/main/set.sh"
+chmod +x /etc/set.sh
+history -c
+echo "1.2" > /home/ver
 clear
 echo " "
-echo '============================================='
-echo "       Installation has been completed!!"
-echo '============================================='
-sleep 3
+echo "Installation has been completed!!"
 echo " "
-echo "=================================-Autoscript Premium-===========================" | tee -a log-install.txt
+echo "============-Autoscript Premium-=============" | tee -a log-install.txt
 echo "" | tee -a log-install.txt
-echo "--------------------------------------------------------------------------------" | tee -a log-install.txt
+echo "---------------------------------------------------" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
 echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
-echo "   - Stunnel4                : 1443, 777"  | tee -a log-install.txt
+echo "   - Stunnel4                : 222, 777"  | tee -a log-install.txt
 echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
-echo "   - WebSocket Dropbear      : 8880" | tee -a log-install.txt
-echo "   - WebSocket OpenSSH       : 2095" | tee -a log-install.txt
-echo "   - WebSocket OpenVPN       : 2082" | tee -a log-install.txt
-echo "   - WebSocket TLS           : 2088" | tee -a log-install.txt
-echo "   - OpenVPN OHP             : 8181"  | tee -a log-install.txt
-echo "   - Dropbear OHP            : 8282"  | tee -a log-install.txt
-echo "   - OpenSSH OHP             : 8383"  | tee -a log-install.txt
-echo "   - Squid Proxy             : 3128, 8080 ,8000 (limit to IP Server)"  | tee -a log-install.txt
+echo "   - Squid Proxy             : 3128, 8080, 8000 (limit to IP Server)"  | tee -a log-install.txt
 echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
 echo "   - Nginx                   : 81"  | tee -a log-install.txt
 echo "   - Wireguard               : 7070"  | tee -a log-install.txt
-echo "   - Wireguard CF            : 2048"  | tee -a log-install.txt
 echo "   - L2TP/IPSEC VPN          : 1701"  | tee -a log-install.txt
 echo "   - PPTP VPN                : 1732"  | tee -a log-install.txt
 echo "   - SSTP VPN                : 444"  | tee -a log-install.txt
@@ -129,28 +93,18 @@ echo "   - SS-OBFS TLS             : 2443-2543"  | tee -a log-install.txt
 echo "   - SS-OBFS HTTP            : 3443-3543"  | tee -a log-install.txt
 echo "   - V2RAY Vmess TLS         : 8443"  | tee -a log-install.txt
 echo "   - V2RAY Vmess None TLS    : 80"  | tee -a log-install.txt
-echo "   - V2RAY Vless TLS         : 8585"  | tee -a log-install.txt
-echo "   - V2RAY Vless None TLS    : 8989"  | tee -a log-install.txt
-echo "   - V2RAY VMESS KCP         : 11063"  | tee -a log-install.txt
-echo "   - XRAY VLESS XTLS         : 6060"  | tee -a log-install.txt
-echo "   - XRAY VLESS SPLICE       : 6060"  | tee -a log-install.txt
-echo "   - XRAY VLESS WS XTLS      : 6060"  | tee -a log-install.txt
-echo "   - XRAY VLESS None TCP     : 6666"  | tee -a log-install.txt
-echo "   - XRAY VLESS TCP          : 6565"  | tee -a log-install.txt
-echo "   - XRAY VMESS NONE TCP     : 6464"  | tee -a log-install.txt
-echo "   - XRAY VMESS TCP          : 6363"  | tee -a log-install.txt
-echo "   - XRAY VMESS KCP          : 6161"  | tee -a log-install.txt
+echo "   - V2RAY Vless TLS         : 2083"  | tee -a log-install.txt
+echo "   - V2RAY Vless None TLS    : 8880"  | tee -a log-install.txt
 echo "   - Trojan                  : 2087"  | tee -a log-install.txt
-echo "   - Trojan GO               : 2096"  | tee -a log-install.txt
-echo "   - Netflix Set ON"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
 echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
-echo "   - Timezone                : Asia/Malaysia (UTC +8)"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
 echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
 echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
 echo "   - IPtables                : [ON]"  | tee -a log-install.txt
-echo "   - Auto-Reboot             : [ON/SET]"  | tee -a log-install.txt
+echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
 echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
-echo "   - Autoreboot On 05.00 UTC +8" | tee -a log-install.txt
+echo "   - Autoreboot On 05.00 GMT +7" | tee -a log-install.txt
 echo "   - Autobackup Data" | tee -a log-install.txt
 echo "   - Restore Data" | tee -a log-install.txt
 echo "   - Auto Delete Expired Account" | tee -a log-install.txt
@@ -158,18 +112,15 @@ echo "   - Full Orders For Various Services" | tee -a log-install.txt
 echo "   - White Label" | tee -a log-install.txt
 echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
+echo "   - Dev/Main                : JOY SMART"  | tee -a log-install.txt
+echo "   - Telegram                 : t.me/joysmark" | tee -a log-install.txt
+echo "   - Instagram                : @trikjoysmart"  | tee -a log-install.txt
+echo "   - Whatsapp                : 0169449960"  | tee -a log-install.txt
+echo "   - Facebook                 : FB Kibo Kajang" | tee -a log-install.txt
+echo "------------------Script Created By Joy Smart-----------------" | tee -a log-install.txt
 echo ""
-echo "   - 1st Dev/Main            : BOTVPN"  | tee -a log-install.txt
-echo "   - Telegram                : t.me/onepiecevpncrew"  | tee -a log-install.txt
-echo ""
-
-sleep 3
-echo '============================================='
-echo '      SISTEM AKAN REBOOT SEBENTAR LAGI'
-echo '============================================='
-echo ""
-rm -f /root/setup.sh
-rm -f /root/.bash_history
-echo " Reboot 15 Sec, Setelah LOGIN, type menu"
-sleep 15
+echo " Reboot 10 Sec"
+sleep 10
+rm -f setup.sh
 reboot
+
